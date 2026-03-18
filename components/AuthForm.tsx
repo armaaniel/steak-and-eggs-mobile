301c69c6690620a84c313db56247fb7f9e052b38
armaaniel <ishmaan@hotmail.com>
2026-03-18 15:57:44 -0600

    test svg position

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -221,7 +221,7 @@ const styles = (colors: typeof Colors.light) => StyleSheet.create({
   },
   logoWrap: {
     position: 'absolute',
-    top: 122,
+    top: 114,
     alignSelf: 'center',
   },
   heading: {

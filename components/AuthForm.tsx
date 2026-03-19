2bb5986a7919f32cd2813fdc29d73f01a717a0f1
armaaniel <ishmaan@hotmail.com>
2026-03-18 19:31:10 -0600

    move logo down

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -221,7 +221,7 @@ const styles = (colors: typeof Colors.light) => StyleSheet.create({
   },
   logoWrap: {
     position: 'absolute',
-    top: 100,
+    top: 106,
     alignSelf: 'center',
   },
   heading: {
